9b209e4ad06eaa8883a1662d7a69f4f2dd2fb8d9
Andrew Yang <ayang14@illinois.edu>
2015-08-01 22:42:27 -0500

    Footer link paths placeholders

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,22 @@ router.get('/', function (req, res) {
 	res.render('index', { title: 'Fantasy Smash Bros' });
 });
 
+router.get('/about', function (req, res) {
+	res.send({message: "About page not implemented yet."});
+});
+
+router.get('/contribute', function (req, res) {
+	res.send({message: "Contribute page not implemented yet."});
+});
+
+router.get('/terms', function (req, res) {
+	res.send({message: "Terms page not implemented yet."});
+});
+
+router.get('/contact', function (req, res) {
+	res.send({message: "Contact page not implemented yet."});
+});
+
 router.get('/flairs', function (req, res) {
 	fs.readFile("./data/flairs.json", function (err, data) {
 		if (err) throw err;
